5d4ec0a7185dc00ada16697fac917a682f078dd4
Leo <leonardodi.gutierrez@gmail.com>
2021-11-30 14:21:36 -0800

    pushing the structure of how the javascipt will look

---
+++ script.js
@@ -0,0 +1,15 @@
+// Variables for selecting html elements 
+var searchList = docuemnt.getElementById("searchList");
+var tempText = document.createElement("li")
+var humidText = document.createElement("li")
+var tempText = document.createElement("li")
+var tempText = document.createElement("li")
+
+// Search from history
+
+// Seach Input 
+
+// Create function to display on dashboard 
+
+// Main data to get and display 
+
