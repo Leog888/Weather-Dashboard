2dc0dccb5fe06732b2013f8efeabb1eaff118372
Leo <leonardodi.gutierrez@gmail.com>
2021-11-30 16:04:19 -0800

    completed history function and tested

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ var tempText = document.createElement("li")
 var tempText = document.createElement("li")
 
 // Search from history
+function history(location){
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=7a35162ff8a1ab9814d70cac9d534864"
+    console.log("histoy: "+ url);
 
+    Display(url)
+}
 // Seach Input 
+function searchInput (){
+    
+}
 
 // Create function to display on dashboard 
 
